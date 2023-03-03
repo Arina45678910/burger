2d80e536a2157e738e6d61b5bf2afdfb7abf1162
Arina45678910 <98322954+Arina45678910@users.noreply.github.com>
2023-03-03 18:09:39 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 let parent = document.querySelector('.nav__list');
 let element = document.querySelectorAll('.nav__item');
 let link = document.querySelector('.nav__link');
-let btn = document.querySelector('.btn');
-let form = document.querySelector('.form');
+let orderNow = document.querySelector('#orderNow');
+let myForm = document.querySelector('.form');
+let productImg = document.querySelector('.productimg')
+let close = document.querySelector('.form_close')
 
 parent.addEventListener('click', function (event) {
     console.log(event.target)
     let currentElement = event.target;
     for (let el of element) {
-        el.parentElement.classList.remove('active');
+        el.classList.remove('active');
         currentElement.parentElement.classList.add('active');
     }
 })
 
+orderNow.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    myForm.classList.toggle('active')
+})
 
-btn.addEventListener('click', function () {
-    document.getElementById('form').style.display = 'block'
+
+
+productImg.addEventListener('click', function (eee) {
+    eee.preventDefault();
+    productImg.classList.toggle('active')
 })
